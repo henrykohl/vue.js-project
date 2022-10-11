577f37293555c0cfb8abc39d3914363909f6b18f
TAYUAN <u860218@gmail.com>
2022-10-11 03:43:06 -0500

    Add Store

--- README.md
+++ README.md
@@ -1,24 +1,57 @@
 # vue.js-playground
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Self Test Notes
+
+READ
+window.localStorage.setItem('todo-vue','[123]')
+$vm0.$store.dispatch('READ_TODOS')
+$vm0.$store.dispatch('READ_TODOS').then((result)=>{ console.log(result) })
+
+CREATE
+$vm0.$store.dispatch('CREATE_TODO',{todo:456})
+
+UPDATE
+window.localStorage.setItem('todo-vue','[123,456,789]')
+$vm0.$store.dispatch('READ_TODOS').then((result)=>{ console.log(result) })
+$vm0.$store.dispatch('UPDATE_TODO',{tId:1,todo:999}).then((result)=>{console.log(result) })
+
+DELETE
+$vm0.$store.dispatch('DELETE_TODO',{tId:1}).then((result)=>{console.log(result)})
+
+ACCESS ALL ITEMS
+window.localStorage.getItem('todo-vue')
+
+---
+
+window.localStorage.setItem('todo-vue',JSON.stringify([{ content: 123, done: false }, { content: 456, done: true }, { content: 789, done: false }]))
+$vm0.$store.dispatch('READ_TODOS').then((result)=>{ console.log(result) })
+
+$vm0.$store.dispatch("UPDATE_TODO",{tId:1, todo:{content:999,done:true}})
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,146 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+/*eslint-disable*/
+import Vue from "vue";
+import Vuex from "vuex";
+import LocalStorage from "../modules/localStorage";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const STORE = LocalStorage("todo-vue"); // new LocalStorage("todo-vue");
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      { content: 123, done: false },
+      { content: 456, done: false },
+      { content: 789, done: false },
+    ],
+    // todos: [],
   },
   getters: {
+    //原始資料轉為有id的模式 (現在沒有後端DB，list去做出把資料id拿出來)
+    list(state) {
+      return state.todos.map((todo, tId) => {
+        return {
+          tId,
+          todo,
+        };
+      });
+    },
+    filterList(state, getters) {
+      return (filter) => {
+        let status = null;
+        switch (filter) {
+          case "all":
+            return getters.list;
+          case "active":
+            status = false;
+            break;
+          case "done":
+            status = true;
+            break;
+          // case 'active':
+          //   return getters.list.filter((todo) => {return !todo.todo.done})
+          // case 'done':
+          //   return getters.list.filter((todo) => {return todo.todo.done})
+          // default:
+          //   return []
+        }
+        return getters.list.filter((todo) => {
+          return todo.todo.done === status;
+        });
+      };
+    },
+    // filterList1 (state) {
+    //   return (filter) => {
+    //   }
+    // or return function(filter){
+    // }
+    // },
+    // filterList2: (state) => (filter) => {
+    // }
   },
   mutations: {
+    SET_TODOS(state, todos) {
+      state.todos = todos;
+    },
   },
   actions: {
+    CREATE_TODO({ commit }, { todo }) {
+      // 1. POST //if使用API 就只需要 axios.post()
+      const todos = STORE.load();
+      todos.push(todo);
+      STORE.save(todos);
+      // 2. commit mutation
+      commit("SET_TODOS", todos);
+      // 3. return
+      return {
+        tId: todos.length - 1,
+        todo,
+      };
+    },
+    READ_TODOS({ commit }) {
+      // 1. load (讀取) const todos = window.localStorage.getItem("XXX");
+      const todos = STORE.load();
+      // 2. commit mutation (寫入 state) commit("SET_TODOS", todos);
+      commit("SET_TODOS", todos);
+      // 3. return return { todos };
+      return {
+        todos,
+      };
+    },
+    // UPDATE方法一:整包傳入，更新，後整包寫回
+    UPDATE_TODO({ commit }, { tId, todo }) {
+      // 1. PATCH //if使用API 可以用到 axios.patch()
+      const todos = STORE.load();
+      todos.splice(tId, 1, todo);
+      STORE.save(todos);
+      // 2. commit mutation
+      commit("SET_TODOS", todos);
+      // 3. return
+      return {
+        tId,
+        todo,
+      };
+    },
+    // UPDATE方法二: 只傳內容content
+    // UPDATE_TODO({ commit, state }, { tId, content }) {
+    //   // 1. PATCH axios.patch()
+    //   if (state.todos[tId].content === content) return;
+    //   console.log("API");
+    //   const todos = STORE.load();
+    //   // todos.splice(tId, 1, todo)
+    //   todos[tId].content = content;
+    //   STORE.save(todos);
+    //   // 2. commit mutation
+    //   commit("SET_TODOS", todos);
+    //   // 3. return
+    //   return {
+    //     tId,
+    //     todo: todos[tId],
+    //   };
+    // },
+    DELETE_TODO({ commit }, { tId }) {
+      // 1. DELETE //if使用API 就只需要 axios.delete()
+      const todos = STORE.load();
+      const todo = todos.splice(tId, 1)[0];
+      STORE.save(todos);
+      // 2. commit mutation
+      commit("SET_TODOS", todos);
+      // 3. return
+      return {
+        tId: null,
+        todo,
+      };
+    },
+    CLEAR_TODOS({ commit }) {
+      const todos = [];
+      STORE.save(todos);
+      commit("SET_TODOS", todos);
+      return {
+        todos,
+      };
+    },
   },
-  modules: {
-  }
-})
+  // modules: {
+  // }
+});
